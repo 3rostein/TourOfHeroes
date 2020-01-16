d9c1cb90ba84cf5380e6bef7a924af9b1c67f2ce
HoldenStein <holdenstein17@gmail.com>
2020-01-16 15:06:11 -0600

    Added the ability to delete heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,44 +1,51 @@
-import { Injectable, ɵCompiler_compileModuleSync__POST_R3__ } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { Hero } from './hero';
+import {
+  Injectable,
+  ɵCompiler_compileModuleSync__POST_R3__
+} from "@angular/core";
+import { Observable, of } from "rxjs";
+import { Hero } from "./hero";
 // import { HEROES } from './mock-heroes';
-import { MessageService } from './message.service';
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { MessageService } from "./message.service";
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from "angularfire2/firestore";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-
 export class HeroService {
   db = firebase.firestore();
 
-  docRef = this.db.collection('heroes');
+  docRef = this.db.collection("heroes");
 
-  constructor(private messageService: MessageService) {
-  }
-
-
+  constructor(private messageService: MessageService) {}
 
   addHero(addName: string): void {
-    if (addName === '' || addName === null) {
-      console.log('Please enter a value');
+    if (addName === "" || addName === null) {
+      console.log("Please enter a value");
     } else {
       const docData = {
-        name: addName,
+        name: addName
       };
       this.messageService.add(`Hero Service: added ${docData.name}`);
-      this.db.collection('heroes').doc().set(docData);
+      this.db
+        .collection("heroes")
+        .doc()
+        .set(docData);
     }
   }
 
   getHeroes(): Observable<Hero[]> {
-    const heroArr: Hero[] = [];
+    let heroArr: Hero[] = [];
 
-    this.docRef.onSnapshot((heroes) => {
+    this.docRef.onSnapshot(heroes => {
+      // heroArr = [];
       heroes.docs.forEach(element => {
-        let hero: Hero = { id: element.id, name: element.get('name') };
+        let hero: Hero = { id: element.id, name: element.get("name") };
 
         heroArr.push(hero);
       });
@@ -49,9 +56,9 @@ export class HeroService {
   getLimitedHeroes(limit: number): Observable<Hero[]> {
     const heroArr: Hero[] = [];
 
-    this.docRef.limit(limit).onSnapshot((heroes) => {
+    this.docRef.limit(limit).onSnapshot(heroes => {
       heroes.docs.forEach(element => {
-        let hero: Hero = { id: element.id, name: element.get('name') };
+        let hero: Hero = { id: element.id, name: element.get("name") };
         heroArr.push(hero);
       });
     });
@@ -61,29 +68,38 @@ export class HeroService {
   async getHero(id: string): Promise<Observable<Hero>> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
 
-    let hero: Hero = await this.db.collection('heroes').doc(id).get().then((doc) => {
-
-      const newHero: Hero = {
-        id: doc.id,
-        name: doc.get('name')
-      };
-      return newHero;
-    });
+    let hero: Hero = await this.db
+      .collection("heroes")
+      .doc(id)
+      .get()
+      .then(doc => {
+        const newHero: Hero = {
+          id: doc.id,
+          name: doc.get("name")
+        };
+        return newHero;
+      });
 
     return of(hero);
-
   }
 
   updateHero(id: string, newName: string): void {
-    this.messageService.add(`Hero Service: updated hero id: ${id} name: ${name}`);
+    this.messageService.add(
+      `Hero Service: updated hero id: ${id} name: ${name}`
+    );
 
-    this.db.collection('heroes').doc(id).update({
-      name: newName
-    });
+    this.db
+      .collection("heroes")
+      .doc(id)
+      .update({
+        name: newName
+      });
   }
 
-
-
-
-
+  deleteHero(id: string) {
+    this.db
+      .collection("heroes")
+      .doc(id)
+      .delete();
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  constructor(private heroService: HeroService) {
-
-  }
+  constructor(private heroService: HeroService) {}
 
   heroes: Hero[];
 
@@ -21,11 +19,14 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  deleteHero(id: string): void {
+    this.heroService.deleteHero(id);
+  }
 }
